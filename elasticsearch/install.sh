6b1ed22def8eaf832d1a0b4c0c153e0294cb2cf6
Anton Sherkhonov <asherkho@redhat.com>
2017-03-31 15:11:12 +0000

    Elasticsearch 5.3.1 without SearchGuard

--- elasticsearch/install.sh
+++ elasticsearch/install.sh
@@ -3,10 +3,13 @@
 set -ex
 
 mkdir -p ${HOME}
+cd ${HOME}/
+yum -y install https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${ES_VER}.rpm
+
+
 ln -s /usr/share/elasticsearch /usr/share/java/elasticsearch
 
-/usr/share/elasticsearch/bin/plugin install io.fabric8/elasticsearch-cloud-kubernetes/${ES_CLOUD_K8S_VER}
-/usr/share/elasticsearch/bin/plugin install io.fabric8.elasticsearch/openshift-elasticsearch-plugin/${OSE_ES_VER}
+/usr/share/elasticsearch/bin/elasticsearch-plugin install io.fabric8:elasticsearch-cloud-kubernetes:${ES_CLOUD_K8S_VER}
 
 
 mkdir /elasticsearch
@@ -14,6 +17,7 @@ mkdir -p $ES_CONF
 chmod -R og+w $ES_CONF
 chmod -R og+w /usr/share/java/elasticsearch ${HOME} /elasticsearch
 chmod -R o+rx /etc/elasticsearch
+<<<<<<< ff7e56f6b03416e9638561f2b72beab2c0d7f041
 chmod +x /usr/share/elasticsearch/plugins/openshift-elasticsearch/sgadmin.sh
 
 PASSWD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 20 | head -n 1)
@@ -22,4 +26,5 @@ cat > ${HOME}/sgconfig/sg_internal_users.yml << CONF
   $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1):
     hash: $PASSWD
 CONF
+
 
