d6d057df950bd0af26eb751ad5198cc132ce530c
Anton Sherkhonov <sherkhonov@gmail.com>
2017-11-08 20:34:12 +0000

    logstash role and user

--- elasticsearch/install.sh
+++ elasticsearch/install.sh
@@ -27,7 +27,7 @@ chmod +x ${ES_HOME}/plugins/openshift-elasticsearch/sgadmin.sh
 
 # document needed by sg plugin to properly initialize
 passwd=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 20 | head -n 1)
-cat > ${HOME}/sgconfig/sg_internal_users.yml << CONF
+cat >> ${HOME}/sgconfig/sg_internal_users.yml << CONF
 ---
   $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1):
     hash: $passwd
